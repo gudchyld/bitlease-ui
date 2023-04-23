70fd8b5ead442ebf9fec78eeb6593f88e2c6ad1c
Elendu Ifeanyi <goodchild66@gmail.com>
2023-04-23 17:46:23 +0100

    Made navbar responsive

---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,25 @@
+// import { slide as Menu } from 'react-burger-menu';
+// import { useState } from 'react';
+
+
+// const HamburgerMenu = () => {
+
+//     const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+//     const handleMenuClick = () => setIsMenuOpen(!isMenuOpen);
+//     return ( 
+//         <Menu isOpen={isMenuOpen} onStateChange={(state) => setIsMenuOpen(state.isOpen)}>
+//         <a className="menu-item" href="/">
+//           Home
+//         </a>
+//         <a className="menu-item" href="/about">
+//           Team
+//         </a>
+//         <a className="menu-item" href="/contact">
+//           Open App
+//         </a>
+//       </Menu>
+//      );
+// }
+ 
+// export default HamburgerMenu;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,41 @@
+import {FaBars, FaTimes} from 'react-icons/fa';
+import { useRef } from 'react';
+import '../styles/navbar.css'
+
+
 const Navbar = () => {
+    const navRef = useRef<null | HTMLUListElement>(null);
+
+    const showNav = () => {
+        navRef.current?.classList.toggle('responsive-nav');
+    }
+
     return (  
-        <div className="bg-[#2C3333] ">
-            <div className="flex items-center  text-white py-8 max-w-[1024px] mx-[auto]"> 
-                <div>
+        <div className="bg-[#2C3333] nav-container">
+            <nav className="nav-holder flex items-center justify-between text-white py-8 max-w-[1024px] mx-[auto]"> 
+            
+                <div className='h2-holder'>
                     <h2 className="text-2xl">Bitlease</h2>
                 </div>
 
-                <div className="flex ml-[auto] items-center" >
-                    <p>Team</p>
+                <ul className="nav-ul flex ml-[auto] items-center" ref={navRef} >
+                    <li>
+                        <a href="#">Team</a>
+                    </li>
+                    <li>
                     <button className="ml-6 border-[#0E8388] border-2 py-2 px-4 rounded-xl">Open App</button>
-                </div>
-            </div>
+                    </li>
+
+                    {/* close button */}
+                    <button onClick={showNav} className='nav-btn nav-btn-close'><FaTimes /></button>
+                </ul>
+
+               {/* put open button here */}
+                <button onClick={showNav} className='nav-btn'><FaBars /></button>
+            </nav>
         </div>
 
     );
 }
  
-export default Navbar;
+export default Navbar; 
